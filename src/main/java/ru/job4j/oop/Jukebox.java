21b88fde9191a18db50bdf3d612a84c62dacbda5
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-03 15:09:25 +0400

    1

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+    public void music(int position) {
+            System.out.println(position);
+        }
+
+        public static void main(String[] args) {
+            Student petya = new Student();
+            if (int position = 1){
+
+            }
+
+
+
+            petya.music();
+        }
+}
