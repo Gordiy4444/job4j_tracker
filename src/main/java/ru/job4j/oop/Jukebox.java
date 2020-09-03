3fca0179a8717a65f3e942e717b1ed256186ba73
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-03 15:40:24 +0400

    1

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -2,17 +2,21 @@ package ru.job4j.oop;
 
 public class Jukebox {
     public void music(int position) {
+
             System.out.println(position);
         }
 
         public static void main(String[] args) {
-            Student petya = new Student();
-            if (int position = 1){
-
+            Student vasya = new Student();
+          if (int position = 1){
+                System.out.println("Пусть бегут неуклюже");
             }
-
-
-
-            petya.music();
+if else (int position = 2){
+                System.out.println("Спокойной ночи");
+            }
+else{
+                System.out.println("Песня не найдена");
+            }
+            vasya.music(1);
         }
 }
