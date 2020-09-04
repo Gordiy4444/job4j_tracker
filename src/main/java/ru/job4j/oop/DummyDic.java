26c363f2fa082fbb5c06f9f51f256c0fa79c97f0
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-04 13:08:10 +0400

    1

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -3,13 +3,13 @@ package ru.job4j.oop;
 public class DummyDic {
     public String  engToRus(String eng) {
         eng = "Известное слово";
-        return eng;
+        return  "Неизвестное слово. " + eng;
     }
 
     public static void main(String[] args) {
         DummyDic dicky = new DummyDic();
         String eng = dicky.engToRus("r");
-        System.out.println("Неизвестное слово. " + eng);
+        System.out.println(eng);
     }
 }
 
