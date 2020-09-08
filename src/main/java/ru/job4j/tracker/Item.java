2a90849015a94a588dcd912715321afc5bf629b7
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-08 11:15:17 +0400

    1

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,15 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Item {
+    private LocalDateTime created;
     private int id;
     private String name;
+    public int now(){
+
+    }
 
     public int getId() {
         return id;
