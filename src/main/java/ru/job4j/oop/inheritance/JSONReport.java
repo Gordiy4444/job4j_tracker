488b554aeded2ee6706226619f3134cba3b4c989
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-10 20:40:12 +0400

    1

---
+++ src/main/java/ru/job4j/oop/inheritance/JSONReport.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop.inheritance;
+
+public class JSONReport extends TextReport{
+    public String generate(String name, String body) {
+        return "name" + name +
+                "body" + body;
+    }
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        String text = report.generate( " :  name ", " : body");
+        System.out.println(text);
+    }
+}
