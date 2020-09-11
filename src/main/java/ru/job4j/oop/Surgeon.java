33a882826781cc9108d4d7872e25638a031eccb8
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-11 08:19:34 +0400

    1

--- src/main/java/ru/job4j/oop/Surgeon.java
+++ src/main/java/ru/job4j/oop/Surgeon.java
@@ -5,11 +5,12 @@ public class Surgeon extends Doctor{
         Profession prof = new Profession();
         System.out.println(prof.getName());
         Doctor doc = new Doctor();
+        Pacient pac = new Pacient();
+        System.out.println(doc.heal(pac));
         System.out.println(doc.getName());
-        System.out.println(doc.heal());
         Surgeon sur = new Surgeon();
         System.out.println(sur.getName());
-        System.out.println(sur.heal());
+        System.out.println(sur.heal(pac));
 
 
     }
