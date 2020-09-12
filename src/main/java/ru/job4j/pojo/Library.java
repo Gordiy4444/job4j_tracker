f5b7ca15f3f93ce3ce5dbbb7fdd8e1f61ae58d04
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-12 11:34:49 +0400

    1

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,28 @@
+package ru.job4j.pojo;
+
+
+public class Library {
+    public static void main(String[] args) {
+        Book bo = new Book("Clean code",500);
+        Book go = new Book("Hed First java", 600);
+        Book so = new Book("Grocking algorithms", 400);
+        Book jo = new Book("Java 8", 700);
+        Book[] boo = new Book[4];
+boo[0] = bo;
+boo[1] = go;
+boo[2] = so;
+boo[3] = jo;
+for (int index = 0; index < boo.length; index ++){
+    Book oo = boo[index];
+    System.out.println(oo.getName() + " - " + oo.getCount());
+}
+        System.out.println("Rearrange 3 from 0");
+        Book bucket = boo[0];
+        boo[0] = boo[3];
+        boo[3] = bucket;
+        for (int index = 0; index < boo.length; index++)
+            Book b = boo[index];
+        System.out.println(oo.getN);
+    }
+
+}
