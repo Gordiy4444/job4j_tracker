d3511bfcf84339aeb4f2477fd6e389b409511b29
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-12 13:37:18 +0400

    1

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -21,6 +21,7 @@ for (int index = 0; index < boo.length; index ++){
         boo[0] = boo[3];
         boo[3] = bucket;
         for (int index = 0; index < boo.length; index++)
+            Book oo = boo[index];
             System.out.println(oo.getName() + " - " + oo.getCount());
     }
 
