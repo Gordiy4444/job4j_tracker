7a19a1292b8ae858c0aa513afe116cd02392f093
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-14 12:21:29 +0400

    1

---
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+import ru.job4j.oop.Product;
+
+    public class ShopDrop {
+        public static Product[] leftShift(Product[] products, int index) {
+            products[index] = products[2];
+            products[index] = null;
+            for (int i = 0; i < products.length; i++) {
+                Product product = products[i];
+                if (product != null) {
+                } else {
+                }
+            }
+            return products;
+        }
+
+        public static void main(String[] args) {
+            Product[] products = new Product[3];
+            products[0] = new Product("Milk", 10);
+            products[1] = new Product("Bread", 4);
+            products[2] = new Product("Egg", 19);
+            for (int i = 0; i < products.length; i++) {
+                Product product = products[i];
+                System.out.println(leftShift(products, 1));
+            }
+        }
+    }
+
+
+
