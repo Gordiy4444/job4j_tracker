9b1d62737dc3d8a74e38d281517a8dfab3504797
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-17 13:46:39 +0400

    1

--- src/main/java/ru/job4j/io/Dendy.java
+++ src/main/java/ru/job4j/io/Dendy.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 public class Dendy {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int run = 11;
+        boolean run = true;
+        int rsl = 11;
         while ( run) {
             System.out.println("Игра");
 
             int select = Integer.valueOf(input.nextLine());
             } if (select == 1) {
-                run = run - 1;
+                rsl = rsl - 1;
+                  if (rsl = 0);{
+                      run = false;
+            }
+
 
             } if (select == 2) {
                 run = run - 2;
