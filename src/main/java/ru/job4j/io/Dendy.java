e1035fb8bd511ec4713e6c0d4b5d37d1caa5200f
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-17 13:37:11 +0400

    1

---
+++ src/main/java/ru/job4j/io/Dendy.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Dendy {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int run = 11;
+        while ( run) {
+            System.out.println("Игра");
+
+            int select = Integer.valueOf(input.nextLine());
+            } if (select == 1) {
+                run = run - 1;
+
+            } if (select == 2) {
+                run = run - 2;
+            } if (select == 3) {
+                run = run - 3;
+
+            }
+
+
+            System.out.println();
+        }
+    }
+}
+
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,26 @@
 package ru.job4j.tracker;
 
-public class StartUI {
-    public static void main(String[] args) {
-        Item item = new Item();
-        Tracker tracker = new Tracker();
-        System.out.println(tracker.add(item));
-        System.out.println(tracker.findById(item.getId()));
+import java.util.Scanner;
 
+public class StartUI {
+
+        public void init(Scanner scanner, Tracker tracker) {
+            boolean run = true;
+            while (run) {
+                this.showMenu();
+                /* if */
+            }
+        }
+
+        private void showMenu() {
+            System.out.println("Menu.");
+            /* добавить остальные пункты меню. */
+        }
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            Tracker tracker = new Tracker();
+            new StartUI().init(scanner, tracker);
+        }
     }
-}
