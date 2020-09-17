2fa468093076f2edecf5a7d2150350f709dc1a93
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-17 07:02:07 +0400

    1

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -65,7 +65,7 @@ public class Tracker {
         int start = index + 1;
         int distPos = index;
         int length = size - index;
-            System.arraycopy(items, index, start, distPos, length);
+            System.arraycopy(items, start,items, distPos, length);
             items[size - 1] = null;
         size--;
         res = true;
