884ccc7076d578baecc23d753ddd2b092a3609cc
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-18 15:04:05 +0400

    1

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,17 +4,34 @@ import java.util.Scanner;
 
 public class StartUI {
 
-        public void init(Scanner scanner, Tracker tracker) {
-            boolean run = true;
-            while (run) {
-                this.showMenu();
-                /* if */
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = false;
+        while (run) {
+            this.showMenu();
+            System.out.print("Select: ");
+            int select = Integer.valueOf(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new Item ====");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+            } else if (select == 1) {
+                tracker.add(item);
+            } else if (select == 6) {
+                run = false;
             }
         }
-
+    }
         private void showMenu() {
             System.out.println("Menu.");
-            /* добавить остальные пункты меню. */
+            System.out.println("0 Add new Item");
+            System.out.println("1. Show all items");
+            System.out.println("2. Edit item");
+            System.out.println("3. Delete item");
+            System.out.println("4. Find item by Id");
+            System.out.println("5. Find items by name");
+            System.out.println("6. Exit Program");
         }
 
 
