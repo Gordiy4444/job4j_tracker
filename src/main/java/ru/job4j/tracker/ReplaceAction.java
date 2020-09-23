49a20f96af9eea5412eb568efe52be0cd45674dd
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-23 11:54:44 +0400

    1

--- src/main/java/ru/job4j/tracker/ReplaceAction.java
+++ src/main/java/ru/job4j/tracker/ReplaceAction.java
@@ -2,15 +2,22 @@ package ru.job4j.tracker;
 
 public class ReplaceAction implements UserAction {
     @Override
-    public boolean execute(Input input, Tracker tracker) {
-        int id = Integer.parseInt(input.askStr("id"));
-        String name = input.askStr("name");
-        Item it = new Item(name);
-        if (tracker.replace(id, it)) {
-            System.out.println("Операция успешна");
-        } else {
-            System.out.println("Операция не успешна");
-        }
-        return true;
+    public String name(){
+
     }
-}
+
+
+        @Override
+        public boolean execute(Input input, Tracker tracker) {
+            int id = Integer.parseInt(input.askStr("id"));
+            String name = input.askStr("name");
+            Item it = new Item(name);
+            if (tracker.replace(id, it)) {
+                System.out.println("Операция успешна");
+            } else {
+                System.out.println("Операция не успешна");
+            }
+            return false;
+        }
+    }
+
