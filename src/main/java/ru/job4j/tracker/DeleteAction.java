38d3e8c14585c6882d0ad917255f94e257634ab0
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-23 12:17:33 +0400

    1

---
+++ src/main/java/ru/job4j/tracker/DeleteAction.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+public class DeleteCreate implements UserAction {
+    @Override
+    public String name() {
+        return null;
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        int idDel = Integer.parseInt(input.askStr("idDel"));
+        if (tracker.delete(idDel)) {
+            System.out.println("Оперция успешна");
+        } else {
+            System.out.println("Оперция не успешна");
+        }
+
+        return false;
+    }
+}
