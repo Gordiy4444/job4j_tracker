046c148b25f377b7b8e00c2a4461be8e94070191
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-26 10:47:22 +0400

    1

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -11,7 +11,7 @@ public class StartUITest {
     @Test
     public void whenCreateItem() {
         Input in = new StubInput(
-                new String[] {"0", "Item name", "1"}
+                new String[]{"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         Output put = new ConsoleOutput();
@@ -31,7 +31,7 @@ public class StartUITest {
         Item item = tracker.add(new Item("Replaced item"));
         String replacedName = "New item name";
         Input in = new StubInput(
-                new String[] {"0",String.valueOf(item.getId()),replacedName, "1"}
+                new String[]{"0", String.valueOf(item.getId()), replacedName, "1"}
         );
         Output put = new ConsoleOutput();
         UserAction[] actions = {
@@ -50,7 +50,7 @@ public class StartUITest {
         Item item = tracker.add(new Item("Deleted item"));
 
         Input in = new StubInput(
-                new String[] {"0" , String.valueOf(item.getId()), "1"}
+                new String[]{"0", String.valueOf(item.getId()), "1"}
         );
         Output put = new ConsoleOutput();
         UserAction[] actions = {
@@ -61,4 +61,22 @@ public class StartUITest {
         new StartUI(output).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
+
+    @Test
+    public void whenFindAll() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Find"));
+        Input in = new StubInput(
+                new String[]{"0", "1"}
+        );
+        Output put = new ConsoleOutput();
+        UserAction[] actions = {
+                new FindAllAction(put),
+                new ExitAction(put)
+        };
+
+        Output output = new ConsoleOutput();
+        new StartUI(output).init(in, tracker, actions);
+        assertThat(tracker.add(), is());
+ }
 }
