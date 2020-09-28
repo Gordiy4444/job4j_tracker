269009da2fa21009a02e9a69c03b34351fe15581
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-28 10:41:26 +0400

    1

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,19 +2,19 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) {
-        if (value[index] != key)
-            throw new ElementNotFoundException("it is impossible without an element");
-
-
-        int rsl = 0;
+        int rsl = -1;
         for (int index = 0; index < value.length; index++) {
-            if (value[index] == key) {
+            if (value[index].equals(key)) {
                 rsl = index;
-                break;
             }
         }
         return rsl;
+        break;
+    }
+            if (rsl == -1){
+        throw new ElementNotFoundException("it is impossible without an element");
+    }
     }
 
-    }
+
 
