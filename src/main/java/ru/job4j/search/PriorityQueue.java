ad8ea11e42c6db4a7ee065e25ca66000a0978160
Gordiy4444 <vazhnoe01@gmail.com>
2020-11-03 13:33:53 +0400

    1

---
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,32 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+
+    public void put(Task task) {
+        int index = 0;
+        for (Task element : tasks) {
+            if ()
+
+            System.out.println(index);
+        }
+
+        this.tasks.add(index, task);
+    }
+
+
+    public Task take() {
+        return tasks.poll();
+    }
+
+
+    public static void main(String[] args) {
+        PriorityQueue one = new PriorityQueue();
+        one.put(new Task("Read", 1));
+        one.put(new Task("Draw", 2));
+        one.take();
+    }
+    }
