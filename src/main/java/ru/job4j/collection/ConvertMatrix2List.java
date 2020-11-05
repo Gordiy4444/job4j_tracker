71e0554ed5669c1a95d6c7f8b28a41dde4351e44
Gordiy4444 <vazhnoe01@gmail.com>
2020-11-05 10:10:51 +0400

    1

--- src/main/java/ru/job4j/collection/ConvertMatrix2List.java
+++ src/main/java/ru/job4j/collection/ConvertMatrix2List.java
@@ -8,9 +8,20 @@ public class ConvertMatrix2List {
         List<Integer> list = new ArrayList<>();
         for (int[] row : array) {
             for (int cell : row) {
-               list =+ cell
+                list.add(cell);
             }
         }
         return list;
+    }
+
+
+    public static void main(String[] args) {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> result = list.toList(input);
+        System.out.println(result);
     }
 }
