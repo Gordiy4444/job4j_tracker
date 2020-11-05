a2a874b4857661ee600a6edaaea103ddf02e6b40
Gordiy4444 <vazhnoe01@gmail.com>
2020-11-05 20:34:14 +0400

    1

--- src/main/java/ru/job4j/collection/ConvertList.java
+++ src/main/java/ru/job4j/collection/ConvertList.java
@@ -6,10 +6,20 @@ import java.util.List;
 public class ConvertList {
     public static List<Integer> convert(List<int[]> list) {
         List<Integer> rsl = new ArrayList<>();
-        for (int row : rsl) {
+        for (int[] row : list) {
             for (int cell : row) {
                 rsl.add(cell);
             }
+        }
         return rsl;
+    }
+
+
+    public static void main(String[] args) {
+        List<int[]> in = new ArrayList<>();
+        in.add(new int[] {1});
+        in.add(new int[] {2, 3});
+        in.add(new int[] {0, 8});
+        System.out.println(convert(in));
     }
 }
