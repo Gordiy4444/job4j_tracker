7fb0585574058d134ca514f4ec382c53d1662eab
Gordiy4444 <vazhnoe01@gmail.com>
2020-11-15 19:04:53 +0400

    1

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,21 +1,21 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Tracker {
-    private final List<Item> items = new Item[100];
+    private final List<Item> items = new ArrayList<>();
     private int ids = 1;
     private int size = 0;
 
     public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
+        items.add(item);
         return item;
     }
 
     public Item[] findAll() {
-        return Arrays.copyOf(items, size);
+        return items.toArray(new Item[0]);
     }
 
     public Item[] findByName(String key) {
@@ -34,13 +34,13 @@ public class Tracker {
 
     public Item findById(int id) {
         int index = indexOf(id);
-        return index != -1 ? items[index] : null;
+        return index != -1 ?  items.get(index) : null;
     }
 
     private int indexOf(int id) {
         int rsl = -1;
         for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
+            if (items.getId(index) == id) {
                 rsl = index;
                 break;
             }
@@ -53,7 +53,7 @@ public class Tracker {
         boolean rsl = index != -1;
         if (rsl) {
             item.setId(id);
-            items[index] = item;
+            items.add(item);
             rsl = true;
         }
         return rsl;
