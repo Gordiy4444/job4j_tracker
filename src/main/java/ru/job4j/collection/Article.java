8a046eb435edf53d69476673c7f910d8b1fc897a
Gordiy4444 <vazhnoe01@gmail.com>
2020-12-08 12:13:31 +0400

    1

---
+++ src/main/java/ru/job4j/collection/Article.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+public class Article {
+    public static boolean generateBy(String origin, String line) {
+        String[] a = origin.split(" ");
+        String[] b = line.split(" ");
+        for (String c : a) {
+             c.equals(b);
+        }
+                return true;
+
+    }
+}
+
