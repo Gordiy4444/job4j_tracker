fbe8a217557d05c012ae0177ad1a6d81e8595d29
Gordiy4444 <vazhnoe01@gmail.com>
2020-12-11 12:55:51 +0400

    1

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -5,12 +5,48 @@ import java.util.HashSet;
 public class FreezeStr {
     public static boolean eq(String left, String right) {
         boolean rsl = true;
-        char[] t = left.toCharArray();
-        char[] g = right.toCharArray();
-        HashSet<> ab = new HashSet<>();
-        for (char e : t) {
-            ab.add(e);
-            }
+        char[] a = left.toCharArray();
+        char[] b = right.toCharArray();
+        String c = a.toString();
+        String v = b.toString();
+        HashSet<String> check = new HashSet<>();
+        for (String ab : c) {
+            check.add(ab);
         }
+
+        for (String vg : v)
+            if (!check.contains(vg)) {
+                rsl = false;
+                break;
+            }
+        return rsl;
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
