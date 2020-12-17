23045a0ccecffab2343178d8cb9504fd8cb8aab9
Gordiy4444 <vazhnoe01@gmail.com>
2020-12-17 14:57:00 +0400

    1

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
         private ArrayList<Person> persons = new ArrayList<Person>();
@@ -10,16 +11,19 @@ public class PhoneDictionary {
         }
 
         public ArrayList<Person> find(String key) {
+           Predicate<Person> name = person -> person.getName().contains(key);
+           Predicate<Person> surname = person -> person.getSurname().contains(key);
+           Predicate<Person> adress = person -> person.getAddress().contains(key);
+           Predicate<Person> phone = person -> person.getPhone().contains(key);
+           Predicate<Person> combine =  name.or(surname.or(adress.or(phone)));
             ArrayList<Person> result = new ArrayList<>();
-            for (Person per : persons) {
-                if (per.getName().contains(key) || per.getSurname().contains(key)
-                        || per.getAddress().contains(key) || per.getPhone().contains(key)) {
-                    result.add(per);
+            for (Person person : persons) {
+                if (combine.test(person)) {
+                    result.add(person);
                 }
             }
             return result;
         }
-
     public static void main(String[] args) {
         PhoneDictionary phones = new PhoneDictionary();
         phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
