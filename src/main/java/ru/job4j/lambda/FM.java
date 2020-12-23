79375d3b1f7ca4cc383df8d41466f546f83471ca
Gordiy4444 <vazhnoe01@gmail.com>
2020-12-23 14:52:22 +0400

    1

--- src/main/java/ru/job4j/lambda/FM.java
+++ src/main/java/ru/job4j/lambda/FM.java
@@ -4,12 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class FM {
-    public static void main(String[] args) {
-        List<List<Integer>> matrix = List.of(
-                List.of(7, 9),
-                List.of(10, 4)
-        );
-        System.out.println(matrix.stream().flatMap(e -> e.stream()).collect(Collectors.toList()));
-    }
+         public  List<Integer> con(int[][] matrix){
+            return matrix.stream.flatMap(e -> e.stream()).collect(Collectors.toList());
+         }
     }
 
