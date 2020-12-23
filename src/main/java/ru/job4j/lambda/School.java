3f48569c66069d336d8dffb7f3812db4cb1211f9
Gordiy4444 <vazhnoe01@gmail.com>
2020-12-23 11:25:51 +0400

    1

--- src/main/java/ru/job4j/lambda/School.java
+++ src/main/java/ru/job4j/lambda/School.java
@@ -6,15 +6,21 @@ import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class School {
-    public Map<String, Student> collect(List<Student> students, Predicate<Student> predict) {
-        return students.stream()
-                .filter(predict)
-                .collect(Collectors.toMap(
-                        e -> e.getSurname(),
-                        e - > e)
-                        );
+import static java.util.stream.Collectors.toMap;
 
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        return students.stream().filter(predict).collect(Collectors.toList());
+    }
+       
+    public static Map<String, Student> sortMap(List<Student> list) {
+        return list.stream().collect(toMap(
+                e -> e.getSurname(),
+                e -> e,
+                (first, second) -> first)
+        );
 
     }
-}
+
+    }
+
