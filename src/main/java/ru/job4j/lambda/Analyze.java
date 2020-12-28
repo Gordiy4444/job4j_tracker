d85072d6064239e1d480e182fdd564fc3ccc1cd3
Gordiy4444 <vazhnoe01@gmail.com>
2020-12-28 16:00:46 +0400

    1

--- src/main/java/ru/job4j/lambda/Analyze.java
+++ src/main/java/ru/job4j/lambda/Analyze.java
@@ -27,12 +27,18 @@ public class Analyze {
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
         return stream.flatMap(x -> x.getSubjects().stream())
                 .collect(Collectors.groupingBy(Subject::getName, Collectors.averagingDouble(Subject::getScore)))
-                .entrySet().stream().map(Map.Entry::getKey, Map.Entry::getValue);
+                .entrySet().stream().map(e -> new Tuple(e.getKey(), e.getValue()))
+                .collect(Collectors.toList());
 
     }
 
     public static Tuple bestStudent(Stream<Pupil> stream) {
-        return null;
+        return stream.map(x -> new Tuple(x.getName(), stream.flatMap(y -> y.getSubjects().stream())
+                .mapToInt(Subject::getScore)
+                .sum()
+                .orElse()))
+                .max(Integer::compare);
+
     }
 
     public static Tuple bestSubject(Stream<Pupil> stream) {
