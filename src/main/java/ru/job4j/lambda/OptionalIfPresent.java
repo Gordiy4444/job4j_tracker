c15046869ceef4111cca5dab885ff5c27c5f2e67
Gordiy4444 <vazhnoe01@gmail.com>
2021-01-06 13:42:29 +0400

    1

--- src/main/java/ru/job4j/lambda/OptionalIfPresent.java
+++ src/main/java/ru/job4j/lambda/OptionalIfPresent.java
@@ -1,23 +1,20 @@
 package ru.job4j.lambda;
 
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public class OptionalIfPresent {
     public static void ifPresent(int[] data) {
-             max(data).ifPresent(integer -> System.out.println("Max:" + max(data)));
+             max(data).ifPresent(integer -> System.out.println("Max:" + max(data).get()));
+    }
+    private static Optional<Integer> max(int[] data) {
+        return IntStream.of(data).boxed().max();
     }
 
-    private static Optional<Integer> max(int[] data) {
-        Optional<Integer> in = Optional.empty();
-        int max = data[0];
-        for (int index = 0; index < data.length; index++) {
-            if (data[index] > max) {
-                max = (data[index]);
-                in = Optional.of(max);
-            }
-        }
-        return in;
-    }
 }
+
+
 
 
