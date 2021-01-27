e6d839700a55dd52045a4a774a8114b268b7d2fd
Gordiy4444 <vazhnoe01@gmail.com>
2021-01-27 14:17:52 +0400

    1

--- src/main/java/ru/job4j/lambda/OptionalOfNullable.java
+++ src/main/java/ru/job4j/lambda/OptionalOfNullable.java
@@ -1,13 +1,14 @@
 package ru.job4j.lambda;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class OptionalOfNullable {
     public static Optional<String> findValue(List<String> strings, String value) {
         Optional<String> user = Optional.ofNullable(null);
         for (String st : strings) {
-            if (st.equals(value)) {
+            if  (Objects.equals(st, value))  {
                 user = Optional.ofNullable(st);
             }
         }
